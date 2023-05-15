96d6a667d71dcba8dc530a257e3266b5975d67d9
Chiemerie Ezechukwu <50174206+chiemerieezechukwu@users.noreply.github.com>
2023-05-15 13:58:35 +0200

    Fix: Update pre-release.sh

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -3,4 +3,10 @@
 echo "Pre-release script"
 echo "New release version: $1"
 
-yq '.name = "Testing"' .github/workflows/release.yml > test
+yq '.name = "Testing"' .github/workflows/release.yml > test.yml
+
+git add .
+
+git commit -m "Commit message from workflow"
+
+git push
