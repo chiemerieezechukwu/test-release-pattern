da670e4fe3afca05e512a0ba161e0c386680b4fb
Chiemerie Ezechukwu <50174206+chiemerieezechukwu@users.noreply.github.com>
2023-05-15 14:08:11 +0200

    Fix: Update pre-release.sh

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -5,6 +5,8 @@ echo "New release version: $1"
 
 yq '.name = "Testing"' .github/workflows/release.yml > test.yml
 
+git config --global url."https://chiemerieezechukwu:$2@github.com/chiemerieezechukwu".insteadOf "https://github.com/chiemerieezechukwu" 
+
 git add test.yml
 
 git commit -m "Commit message from workflow"
